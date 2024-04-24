79816b151fe51736de54608fa7de979fbd1bd338
ehsan001 <ehsan011@d.umn.edu>
2024-04-24 10:37:40 -0500

    Add plot for 10000 population

--- plot.py
+++ plot.py
@@ -14,9 +14,9 @@ def read_csv(filename):
 def plot_line_graph(x_values, y_values,label):
     plt.plot(x_values, y_values,label = label)
 
-y_values =  [i for i in range(60)]
-x_best = read_csv('best_csv_500_1713921828.csv')
-x_avg = read_csv('average_csv_500_1713921828.csv')
+y_values =  [i+1 for i in range(60)]
+x_best = read_csv('best_csv_10000_1713922913.csv')
+x_avg = read_csv('average_csv_10000_1713922913.csv')
 print(x_avg)
 print(x_best)
 plot_line_graph(y_values, x_best, 'Best')
@@ -24,7 +24,7 @@ plot_line_graph(y_values, x_avg,'Avg')
 
 plt.xlabel('Generations')
 plt.ylabel('Fitness')
-plt.title('Best and Average for AAAAAAAAAAAAAAAAAAAA')
+plt.title('Best and Average for UGCUAGAAAGCAUGCGGGGA')
 plt.grid(True)
 plt.legend()
 plt.show()
